fa021d3048dc24445e1f630a2c62f25daa5f7bea
ralrobaili <ralrobaili@gmail.com>
2025-10-28 09:40:03 +0300

    Initial Commit

---
+++ Finallplants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finallplants
+//
+//  Created by raghad alenezi on 06/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finallplants/FinallplantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinallplantsApp.swift
+//  Finallplants
+//
+//  Created by raghad alenezi on 06/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct FinallplantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
